8c442bd68b70121330760d250fffe4bd77c0e79f
ridhi <ridhisindwai44@gmail.com>
2018-08-17 22:36:39 +0530

    first commit

---
+++ assignment2.py
@@ -0,0 +1,31 @@
+#Question 1
+a='Hello people'
+print(a)
+
+#Question 2
+str1='ridhi'
+str2='sindwani'
+str3=str1+str2
+print(str3)
+
+#Question 3
+x=int(input("Enter value in first variable"))
+y=int(input("Enter value in second variable"))
+z=int(input("Enter value in third variable"))
+print("First variable is %d, Second variable is %d and Third variable is %d" %(x,y,z))
+
+#Question 4
+val='Let\'s get started'
+print(val)
+
+#Question 5
+s='Acadview'
+course='Python'
+fees=5000
+print("Name: %s , Course: %s , Fees: %d" %(s,course,fees))
+
+#Question 6
+radius=float(input("Enter radius of circle"))
+pi=3.14
+area=pi*(radius**2)
+print("Area of circle with radius " , radius , " is " , area)
